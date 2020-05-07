9927afbfabe531cd40555d1ea06a7abf1e8e8638
Diogo Silveira <diogo.silveira10@ua.pt>
2020-05-07 03:48:49 +0100

    infrastructure + DB

---
+++ bestofbooks/src/main/java/tqs/group4/bestofbooks/BestofbooksApplication.java
@@ -0,0 +1,13 @@
+package tqs.group4.bestofbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestofbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestofbooksApplication.class, args);
+	}
+
+}
---
+++ bestofbooks/src/test/java/tqs/group4/bestofbooks/BestofbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.group4.bestofbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestofbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
