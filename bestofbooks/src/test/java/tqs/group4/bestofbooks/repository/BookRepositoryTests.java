235553a24a25385666ea0d0246c79b48539bf156
gabsw <sw.gabrielasantos@gmail.com>
2020-05-10 17:45:08 +0100

    Adds BookRepositoryTests Changes FK to publisher name and init files

--- bestofbooks/src/test/java/tqs/group4/bestofbooks/repository/BookRepositoryTests.java
+++ bestofbooks/src/test/java/tqs/group4/bestofbooks/repository/BookRepositoryTests.java
@@ -1,11 +1,11 @@
 package tqs.group4.bestofbooks.repository;
 
 import com.google.common.collect.Lists;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -13,20 +13,36 @@ import org.springframework.data.domain.Pageable;
 import tqs.group4.bestofbooks.mocks.BookMocks;
 import tqs.group4.bestofbooks.model.Book;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-@AutoConfigureTestDatabase
-@DataJpaTest
+@SpringBootTest
+@Transactional
 public class BookRepositoryTests {
     @Autowired
-    private TestEntityManager entityManager;
+    private EntityManager entityManager;
 
     @Autowired
     private BookRepository bookRepository;
 
+    @BeforeEach
+    public void before() {
+        entityManager.createNativeQuery("TRUNCATE books, orders, commissions").executeUpdate();
+    }
+
+    @AfterEach
+    public void after() {
+        entityManager.remove(BookMocks.infiniteJest);
+        entityManager.remove(BookMocks.onTheRoad);
+        entityManager.flush();
+    }
+
     @Test
     public void whenFindByExistentIsbn_thenReturnBook() {
-        entityManager.persistAndFlush(BookMocks.onTheRoad);
+        entityManager.persist(BookMocks.onTheRoad);
+        entityManager.flush();
 
         Book queryResults = bookRepository.findByIsbn(BookMocks.onTheRoad.getIsbn());
         assertEquals(BookMocks.onTheRoad, queryResults);
@@ -40,20 +56,25 @@ public class BookRepositoryTests {
 
     @Test
     public void whenFindByQuantityGreaterThan_thenReturnPageOfBook() {
-        entityManager.persistAndFlush(BookMocks.onTheRoad);
-        entityManager.persistAndFlush(BookMocks.infiniteJest);
+        entityManager.persist(BookMocks.onTheRoad);
+        entityManager.persist(BookMocks.infiniteJest);
+        entityManager.flush();
 
         Pageable p = PageRequest.of(0, 20);
         Page<Book> bookPage = new PageImpl<>(Lists.newArrayList(BookMocks.infiniteJest, BookMocks.onTheRoad), p, 2);
 
         Page<Book> queryResults = bookRepository.findByQuantityGreaterThan(0, p);
-        assertEquals(bookPage, queryResults);
+        assertAll("findByQuantityDB",
+                () -> assertTrue(bookPage.getContent().contains(BookMocks.onTheRoad)),
+                () -> assertTrue(bookPage.getContent().contains(BookMocks.infiniteJest))
+        );
     }
 
     @Test
     public void whenSearchFilteredBooks_thenReturnPageOfBook() {
-        entityManager.persistAndFlush(BookMocks.onTheRoad);
-        entityManager.persistAndFlush(BookMocks.infiniteJest);
+        entityManager.persist(BookMocks.onTheRoad);
+        entityManager.persist(BookMocks.infiniteJest);
+        entityManager.flush();
 
         Pageable p = PageRequest.of(0, 20);
         Page<Book> bookPage = new PageImpl<>(Lists.newArrayList(BookMocks.onTheRoad), p, 1);
