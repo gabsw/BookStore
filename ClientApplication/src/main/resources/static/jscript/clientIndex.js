2e362c37024eaa1e37fc6359f6f976ed3c1faefe
Carolina Marques <carolinaresendemarques@ua.pt>
2020-05-21 14:24:00 +0100

    search test done

--- ClientApplication/src/main/resources/static/jscript/clientIndex.js
+++ ClientApplication/src/main/resources/static/jscript/clientIndex.js
@@ -210,23 +210,25 @@ function searchBooksNoLogin() {
     fetch(url+'/books/search?' + add + book_title)
         .then(res => res.json())
         .then((data) => {
+            let count = 0;
             let output = '';
             if ( data["content"].length == 0) { output = '<h1 class="mt-0 font-weight-bold mb-2" align="center">Nothing Found !</h1>';}
             else {
                 data["content"].forEach(function (book) {
+                    count ++;
                     output += `
                 <li class="list-group-item">
                     <!-- Custom content-->
                     <div class="media align-items-lg-center flex-column flex-lg-row p-3">
                         <div class="media-body order-2 order-lg-1">
-                            <h5 class="mt-0 font-weight-bold mb-2">${book["title"]}</h5>
+                            <h5 class="mt-0 font-weight-bold mb-2" id="book_title `+count + `">${book["title"]}</h5>
 
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item" >Author: ${book.author} </li>
-                                <li class="list-group-item">Category: ${book.category} </li>
-                                <li class="list-group-item">Publisher: ${book.publisherName}</li>
-                                <li class="list-group-item">Price: <strong>${book.price}</strong></li>
-                                <li class="list-group-item"><strong>Available:</strong> ${book.quantity}</li>
+                                <li class="list-group-item" id="book_author `+count + `">Author: ${book.author} </li>
+                                <li class="list-group-item" id="book_category`+count + `">Category: ${book.category} </li>
+                                <li class="list-group-item" id="book_pub `+count + `">Publisher: ${book.publisherName}</li>
+                                <li class="list-group-item" id="book_price `+count + `">Price: <strong>${book.price}</strong></li>
+                                <li class="list-group-item" id="book_qt `+count + `"><strong>Available:</strong> ${book.quantity}</li>
                             </ul>
                             <div class="btn-group" style="width:100%">
                                  <a class="btn btn-outline-warning" role="button" id="detailsModal" href="login.html">Login to Buy/See More !</a>
@@ -235,7 +237,7 @@ function searchBooksNoLogin() {
                     </div>
                     <!-- End -->
                 </li>
-                         `;
+                        ` ;
                 })
             }
             document.getElementById('output').innerHTML = output;
@@ -250,14 +252,15 @@ function getBooksNoLogin() {
         .then(res => res.json())
         .then((data) => {
             let output = '';
-
             data["content"].forEach(function (book) {
+                count ++;
+                var book_title = "book_title" + count.toString();
                 output += `
                 <li class="list-group-item">
                     <!-- Custom content-->
                     <div class="media align-items-lg-center flex-column flex-lg-row p-3">
                         <div class="media-body order-2 order-lg-1">
-                            <h5 class="mt-0 font-weight-bold mb-2">${book["title"]}</h5>
+                            <h5 class="mt-0 font-weight-bold mb-2"> ${book["title"]}</h5>
 
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item" >Author: ${book.author} </li>
@@ -296,7 +299,7 @@ function getBooks(){
                     <!-- Custom content-->
                     <div class="media align-items-lg-center flex-column flex-lg-row p-3">
                         <div class="media-body order-2 order-lg-1">
-                            <h5 class="mt-0 font-weight-bold mb-2">${book["title"]}</h5>
+                            <h5 class="mt-0 font-weight-bold mb-2" >${book["title"]}</h5>
 
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item" >Author: ${book.author} </li>
