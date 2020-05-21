579009e47c7b06abd4f9663fb742e441cfbf3fd8
gabsw <sw.gabrielasantos@gmail.com>
2020-05-21 15:59:04 +0100

    Updates external publisher

--- external_publisher/publisher.py
+++ external_publisher/publisher.py
@@ -1,13 +1,53 @@
-from flask import Flask, jsonify
+import requests
+import time
+import base64
 
-app = Flask(__name__)
+
+publisher_name = 'External Publisher'
+username = 'external_publisher'
+password = 'pw'
+basic_token = username + ':' + password
+
+session = requests.Session()
+
+auth_token = None
+
+auth_header = {'Authorization': 'Basic ' + base64.encodebytes(basic_token)}
 
 
-@app.route('/', methods=['GET'])
-def index():
-    greeting = {'hello': 'world'}
-    return jsonify(greeting)
+def login():
+    response = session.post('https://localhost:8080/api/session/login', headers=auth_header)
+    if response.status_code != 200:
+        raise Exception('bad credentials')
+
+
+def get_stock():
+    current_page = 0
+
+    while True:
+        response = session.get(f"https://localhost:8080/api/publisher/{publisher_name}/stock?page={current_page}")
+        book_page = response.json()
+        is_last_page = book_page['last']
+
+        books = book_page['content']
+        for book in books:
+            if book['quantity'] == 0:
+                update_stock(book['isbn'])
+
+        if is_last_page:
+            break
+        current_page += 1
+
+
+def update_stock(isbn):
+    new_stock = {'isbn': isbn, 'quantity': 10}
+    session.put(f"https://localhost:8080/api/publisher/{publisher_name}/stock", json=new_stock)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    while True:
+        login()
+        get_stock()
+        time.sleep(60 * 60)
+
+
