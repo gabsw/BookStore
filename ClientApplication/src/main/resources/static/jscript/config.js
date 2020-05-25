59d5f8b46653de9546d9063dbf0259bf7be0a76f
Carolina Marques <carolinaresendemarques@ua.pt>
2020-05-25 16:29:09 +0100

    added login feature but getting token as null

--- ClientApplication/src/main/resources/static/jscript/config.js
+++ ClientApplication/src/main/resources/static/jscript/config.js
@@ -1,1 +1,13 @@
-let url = 'http://192.168.160.70:8080/api/'
+let url = 'http://192.168.160.70:8080/api/';
+
+function getCurrentUser() {
+    return localStorage.getItem('token');
+}
+
+function clearUser() {
+   localStorage.clear()
+}
+
+function setCurrentToken(token) {
+    localStorage.setItem('token',token);
+}
