8fa8f02241f60dc1597b68cf955019c90eefd14c
gabsw <sw.gabrielasantos@gmail.com>
2020-05-25 21:01:44 +0100

    sonarqube fix

---
+++ scripts/pull_request_test_and_analysis.sh
@@ -0,0 +1,12 @@
+BRANCH_NAME=${GITHUB_REF##*/}
+PR_KEY=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+BASE_BRANCH_NAME=master
+
+echo $BRANCH_NAME
+echo $PR_KEY
+echo $BASE_BRANCH_NAME
+
+
+mvn clean verify sonar:sonar --file bestofbooks/pom.xml -Dsonar.projectKey=gabsw_BookStore \
+	-Dsonar.organization=gabsw -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN \
+	-Dsonar.branch.name=$BRANCH_NAME -Dsonar.pullrequest.key=$PR_KEY -Dsonar.pullrequest.base=master
