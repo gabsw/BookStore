85ba9d85b7c4627643823b9baa3058670c498493
Carolina Marques <carolinaresendemarques@ua.pt>
2020-06-03 19:28:27 +0100

    selenium tests in progress

---
+++ ClientApplication/src/test/selenium/SearchBookIT.java
@@ -0,0 +1,93 @@
+package selenium;
+
+import com.client.application.enduser.EndUserApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = EndUserApplication.class)
+public class SearchBookIT {
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+
+  @Before
+  public void setUp() {
+	ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless", "--disable-gpu");
+    driver = new ChromeDriver(options);
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<>();
+    driver.get("http://localhost:8080/");
+  }
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+
+  @Test
+  public void searchBookTitle(){
+    driver.findElement(By.id("findBook")).click();
+    driver.findElement(By.id("findBook")).sendKeys("Book1");
+    driver.findElement(By.id("book_title")).click();
+    driver.findElement(By.id("search_book")).click();
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    WebElement selectPollutant = wait.until(ExpectedConditions.elementToBeClickable(By.id("book_title 1")));
+    selectPollutant.click();
+    assertThat(driver.findElement(By.id("book_title 1")).getText(), is("Book1"));
+  }
+
+
+  @Test
+  public void searchBookCategory() {
+    driver.findElement(By.id("findBook")).click();
+    driver.findElement(By.id("findBook")).sendKeys("horror");
+    driver.findElement(By.id("book_category")).click();
+    driver.findElement(By.id("search_book")).click();
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    WebElement selectPollutant = wait.until(ExpectedConditions.elementToBeClickable(By.id("book_category 1")));
+    selectPollutant.click();
+    assertThat(driver.findElement(By.id("book_category 1")).getText(), containsString("SciFi"));
+  }
+
+  @Test
+  public void searchBookAuthor(){
+    driver.findElement(By.id("findBook")).click();
+    driver.findElement(By.id("findBook")).sendKeys("someauthor");
+    driver.findElement(By.id("book_author")).click();
+    driver.findElement(By.id("search_book")).click();
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    WebElement selectPollutant = wait.until(ExpectedConditions.elementToBeClickable(By.id("book_author 1")));
+    selectPollutant.click();
+    assertThat(driver.findElement(By.id("book_author 1")).getText(), containsString("someauthor"));
+  }
+
+
+  @Test
+  public void searchInexistingBook() {
+    driver.findElement(By.id("findBook")).click();
+    driver.findElement(By.id("findBook")).sendKeys("nothing");
+    driver.findElement(By.id("search_book")).click();
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    WebElement selectPollutant = wait.until(ExpectedConditions.elementToBeClickable(By.id("undefined")));
+    selectPollutant.click();
+    assertThat(driver.findElement(By.id("undefined")).getText(), is("Nothing Found !"));
+  }
+}
---
+++ ClientApplication/target/classes/static/jscript/publisherRevenue.js
@@ -0,0 +1,56 @@
+function getRevenues(name) {
+    fetch(url+ 'publisher/'+ name+ '/revenue',{
+        method: 'get',
+        headers: {
+            'x-auth-token': getCurrentUser()
+        }
+    })
+        .then(res => res.json())
+        .then((data) => {
+            let count =0 ;
+            let output = '';
+            data["content"].forEach(function (revenue) {
+                count ++;
+                output += `
+                <li class="list-group-item">
+                    <!-- Custom content-->
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5 class="mt-0 font-weight-bold mb-2" id="revenue_title `+count + `">Revenue #${revenue["id"]}</h5>
+
+                            <ul class="list-group list-group-flush">
+                            <li class="list-group-item" id="revenue_amount `+count + `">Amount: ${revenue["amount"]}  &euro;</li>
+                            <li class="list-group-item" id="revenue_order `+count + `">Order ID: ${revenue.orderId} </li>
+                            <li class="list-group-item" id="revenue_isbn `+count + `">ISBN : ${revenue.isbn} </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                 </li>
+
+                    `;
+
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+function getRevenuesTotal(name) {
+    fetch(url + 'publisher/'+ name +'/revenue/total',{
+        method: 'get',
+        headers: {
+            'x-auth-token': getCurrentUser()
+        }
+    })
+
+        .then(res => res.json())
+        .then((data) => {
+            document.getElementById('finalPrice').innerHTML = '<strong>Total: </strong> '+  data +' &euro;';
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
